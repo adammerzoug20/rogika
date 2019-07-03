f37335f26eed8ae36c41127309cc1e00c16ba8a8
adammerzoug20 <50151703+adammerzoug20@users.noreply.github.com>
2019-07-03 15:12:55 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,77 @@
+const Discord = require('discord.js');
+const devs = ['389090790984515594'];
+const db = require('quick.db');
+const premium = ['470896018603376640']
+const client = new Discord.Client();   
+const bot = new Discord.Client();   
+const giphy = require('giphy-api')();    
+const googl = require('goo.gl');  
+const translate = require('google-translate-api');   
+const fs = require("fs"); 
+const canvas = require("canvas");
+const getYoutubeID = require('get-youtube-id'); 
+const moment = require("moment");  
+const { Client, Util } = require('discord.js');  
+const UserBlocked = new Set(); 
+const jimp = require('jimp');   
+const math = require('math-expression-evaluator'); 
+const stripIndents = require('common-tags').stripIndents;
+const figlet = require('figlet');
+const google = require('google-it'); 
+const queue = new Map(); 
+const zalgo = require('zalgolize');   
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const sql = require("sqlite");
+const dateFormat = require('dateformat'); 
+const pretty = require('pretty-ms') 
+const prefix = 'بيضة';
+var table = require('table').table
+var ti={}  
+,spee={}
+,attentions={};
+
+client.on('ready', function(){
+    var ms = 60000 ;
+    var setGame = ['انا بيضة جميلة'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/barontube`);
+    }, ms);
+    console.log(` ????????? |> Name: ${client.user.username}`);
+ console.log(` ????????? |> Servers: ${client.guilds.size}`);
+ console.log(` ???????????????????? |> Members: ${client.users.size}`);
+ console.log(` ????????????????????? |> Channels: ${client.channels.size}`);
+ console.log(` ???????????????????? |> Channels: ${client.channels.size}`);
+ console.log(` ???????????????????? |> Id: ${client.user.id}`);
+ console.log(` ???????????????????`);
+ console.log(` ???????????????????`);
+});
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'كيفية تقشير البيض'))
+    let pages = [`**
+        ***__| كيفية تقشيرالبيض |__***
+**
+تفقص ام البيضة
+--------------------
+تقشر ام البيضة
+---------------------
+اتمنى يكون عجبكم الفديو ولا تنسو تقشرو البيضة واسلام عليكم 
+    }
+})
+client.on('guildMemberAdd', member => {
+    member.createDM().then(function (channel) {
+return channel.send("")
+    }
+    )});
+client.login(process.env.BOT_TOKEN)
